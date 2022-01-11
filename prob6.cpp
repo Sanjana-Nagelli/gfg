79ecfe81b61adc97a10a84317068dbeba26c971e
Sanjana Nagelli <67863203+Sanjana-Nagelli@users.noreply.github.com>
2022-01-11 11:41:48 +0530

    Display longest name

---
+++ prob6.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        for(int i=0;i<n;i++){
+            if(names[0].length()<names[i].length())
+            names[0]=names[i];
+        }
+        return names[0];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+  // } Driver Code Ends
