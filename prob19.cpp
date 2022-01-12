81a66bb51197a4619a2e0d63e19cf02d25cfce88
Sanjana Nagelli <67863203+Sanjana-Nagelli@users.noreply.github.com>
2022-01-12 20:12:00 +0530

    Print first letter of every word in the string

---
+++ prob19.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	string firstAlphabet(string S)
+	{
+	    // Your code goes here
+	    string s = "";
+	    stringstream str(S);
+	    string word;
+	    while(str>>word){
+	        s += word[0];
+	    }
+	    return s;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   	
+        Solution ob;
+   		cout << ob.firstAlphabet(s) << "\n";
+   	}
+
+    return 0;
+} 
+  // } Driver Code Ends
